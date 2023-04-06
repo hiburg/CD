93bca0dc7a185038abfe662e64f0c2e399f3c68d
hiburg <hiburg@xs4all.nl>
2023-04-06 16:42:05 +0200

    first commit

---
+++ test_main.py
@@ -0,0 +1,15 @@
+import pytest
+
+from main import app
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_index(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, this is the CD assignment !"
